68cedecaba73604b61a8ea32e68c3b930363bb71
diegomolv3r <molverdiego@gmail.com>
2025-05-19 16:32:41 -0300

    se agrega sobreDTO

---
+++ src/main/java/com/tallerwebi/presentacion/dto/SobreDTO.java
@@ -0,0 +1,11 @@
+package com.tallerwebi.presentacion.dto;
+
+public class SobreDTO {
+    private String titulo;
+    private Double precio;
+    private TipoSobre rareza;
+    private String descripcion;
+    private String imagenUrl;
+    
+
+}

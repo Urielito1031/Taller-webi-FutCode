29c7bcdd215842f06c7bc6b3d42ad8f1c6b10858
Urielito1031 <urielestebanyurquina@gmail.com>
2025-06-12 17:24:34 -0300

    configuracion para correr el proyecto con docker

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -14,32 +14,33 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class HibernateConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/db_futcode?useSSL=false&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        return dataSource;
-    }
 //    @Bean
 //    public DataSource dataSource() {
 //        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 //        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        // Usa variables de entorno para la conexión
-//        String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "mysql";
-//        String dbPort = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
-//        String dbName = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "db_futcode";
-//        String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
-//        String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "root";
-//
-//        dataSource.setUrl("jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName + "?useSSL=false&serverTimezone=UTC");
-//        dataSource.setUsername(dbUser);
-//        dataSource.setPassword(dbPassword);
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/db_futcode?useSSL=false&serverTimezone=UTC");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
 //        return dataSource;
 //    }
+        @Bean
+        public DataSource dataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
+            String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
+            String dbPort = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
+            String dbName = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "db_futcode";
+            String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
+            // Local: "", Docker: "root"
+            String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "";
+
+            dataSource.setUrl("jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+            dataSource.setUsername(dbUser);
+            dataSource.setPassword(dbPassword);
+
+            return dataSource;
+        }
     @Bean
     public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();

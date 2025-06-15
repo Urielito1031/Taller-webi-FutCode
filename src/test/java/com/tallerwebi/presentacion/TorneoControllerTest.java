02ad070a49ea4e0a626079f686ad89d7b3d89de6
Urielito1031 <urielestebanyurquina@gmail.com>
2025-06-15 16:39:29 -0300

    agregando nuevas pruebas unitarias a torneoController

--- src/test/java/com/tallerwebi/presentacion/TorneoControllerTest.java
+++ src/test/java/com/tallerwebi/presentacion/TorneoControllerTest.java
@@ -1,7 +1,9 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.service.EquipoTorneoService;
 import com.tallerwebi.dominio.service.TorneoService;
 import com.tallerwebi.presentacion.controller.TorneoController;
+import com.tallerwebi.presentacion.dto.EquipoTorneoDTO;
 import com.tallerwebi.presentacion.dto.TorneoDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,9 @@ public class TorneoControllerTest {
 
    @Mock
    private TorneoService torneoService;
+
+   @Mock
+   private EquipoTorneoService equipoTorneoService;
 
    @InjectMocks
    private TorneoController torneoController;
@@ -59,35 +64,57 @@ public class TorneoControllerTest {
       verify(model).addAttribute("mensajeTorneo", "No hay torneos para mostrar");
    }
 
-//   @Test
-//   public void deberiaMostrarDetalleTorneoCuandoSeVerificaUnIdValido(){
-//      Long idTorneo = 1L;
-//
-//      TorneoDTO torneo = new TorneoDTO();
-//      torneo.setId(idTorneo);
-//
-//      when(torneoService.getById(idTorneo)).thenReturn(torneo);
-//
-//
-//      String vistaDetalleTorneo = torneoController.detalleTorneo(idTorneo, model);
-//      verify(torneoService).getById(idTorneo);
-//
-//      verify(model).addAttribute("torneo", torneo);
-//
-//      assertThat(vistaDetalleTorneo,is("detalle-torneo"));
-//   }
-//   @Test
-//   public void deberiaRetornarNullSiElTorneoNoExisteAlVerDetalle() {
-//      Long idInexistente = 99L;
-//      when(torneoService.getById(idInexistente)).thenReturn(null);
-//
-//      String vistaDetalle = torneoController.detalleTorneo(idInexistente, model);
-//
-//      verify(torneoService).getById(idInexistente);
-//
-//      verify(model, never()).addAttribute(eq("torneo"), any(TorneoDTO.class));
-//     assertThat(vistaDetalle, is("detalle-torneo"));
-//
-//   }
+   @Test
+   public void deberiaMostrarDetalleTorneoCuandoSeVerificaUnIdValido(){
+      Long idTorneo = 1L;
+
+      TorneoDTO torneo = new TorneoDTO();
+      torneo.setId(idTorneo);
+
+      when(torneoService.getById(idTorneo)).thenReturn(torneo);
+
+
+      String vistaDetalleTorneo = torneoController.detalleTorneo(idTorneo, model);
+      verify(torneoService).getById(idTorneo);
+
+      verify(model).addAttribute("torneo", torneo);
+
+      assertThat(vistaDetalleTorneo,is("detalle-torneo"));
+   }
+   @Test
+   public void deberiaMostrarDetalleTorneoCuandoSeVerificaUnIdValidoIncluyendoListaEquipos() {
+      Long torneoId = 1L;
+      TorneoDTO torneo = new TorneoDTO( );
+      List<EquipoTorneoDTO> equipoTorneoList = new ArrayList<>();
+
+
+      when(torneoService.getById(torneoId)).thenReturn(torneo);
+      when(equipoTorneoService.getAllByTorneoId(torneoId)).thenReturn(equipoTorneoList);
+
+      String vistaDetalleTorneo = torneoController.detalleTorneo(torneoId, model);
+
+      assertThat(vistaDetalleTorneo, is("detalle-torneo"));
+
+      verify(model).addAttribute("torneo", torneo);
+      verify(model).addAttribute("torneoEquipos", equipoTorneoList);
+
+      verify(torneoService).getById(torneoId);
+      verify(equipoTorneoService).getAllByTorneoId(torneoId);
+
+
+   }
+   @Test
+   public void deberiaRetornarNullSiElTorneoNoExisteAlVerDetalle() {
+      Long idInexistente = 99L;
+      when(torneoService.getById(idInexistente)).thenReturn(null);
+
+      String vistaDetalle = torneoController.detalleTorneo(idInexistente, model);
+
+      verify(torneoService).getById(idInexistente);
+
+      verify(model, never()).addAttribute(eq("torneo"), any(TorneoDTO.class));
+     assertThat(vistaDetalle, is("detalle-torneo"));
+
+   }
 
 }

7f87b6387ad9be50b6b6d4075a4c2211f76963f7
Urielito1031 <urielestebanyurquina@gmail.com>
2025-07-03 20:20:30 -0300

    agregando configuracion en hibernateConfig para correr el proyecto en docker sin problemas para sprint

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -19,14 +19,16 @@ public class HibernateConfig {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
+        String dbHost = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "mysql";
         String dbPort = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
         String dbName = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "db_futcode";
         String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
-        // Local: "", Docker: "root"
-        String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "";
+        String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "root";
 
-        dataSource.setUrl("jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+        String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName +
+          "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&connectTimeout=30000";
+        System.out.println("Conectando a: " + url + " con usuario: " + dbUser + " y contraseña: " + dbPassword);
+        dataSource.setUrl(url);
         dataSource.setUsername(dbUser);
         dataSource.setPassword(dbPassword);
 
@@ -53,6 +55,7 @@ public class HibernateConfig {
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.connection.timeout", "30000");
         return properties;
     }
 }

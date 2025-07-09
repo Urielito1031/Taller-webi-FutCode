3973fe69aabf6631025ca794fb18624f9281c759
Urielito1031 <urielestebanyurquina@gmail.com>
2025-07-09 18:17:23 -0300

    merge con molver pre

---
+++ src/main/java/com/tallerwebi/dominio/service/PartidoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.tallerwebi.dominio.service;
+
+public class PartidoServiceImpl {
+}
---
+++ src/main/java/com/tallerwebi/presentacion/controller/PartidoCotroller.java
@@ -0,0 +1,4 @@
+package com.tallerwebi.presentacion.controller;
+
+public class PartidoCotroller {
+}

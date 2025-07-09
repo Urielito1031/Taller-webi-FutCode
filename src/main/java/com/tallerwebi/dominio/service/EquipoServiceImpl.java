db18a6348a8449270239ba436d077dc4e1ae1cc1
TomasZ-png <animacionesc4d@gmail.com>
2025-07-09 18:50:01 -0300

    metodo rating equipo

--- src/main/java/com/tallerwebi/dominio/service/EquipoServiceImpl.java
+++ src/main/java/com/tallerwebi/dominio/service/EquipoServiceImpl.java
@@ -1,15 +1,14 @@
 package com.tallerwebi.dominio.service;
 
-import com.tallerwebi.dominio.model.entities.Equipo;
-import com.tallerwebi.dominio.model.entities.Esquema;
-import com.tallerwebi.dominio.model.entities.Jugador;
-import com.tallerwebi.dominio.model.entities.Usuario;
+import com.tallerwebi.dominio.model.entities.*;
 import com.tallerwebi.dominio.repository.EquipoRepository;
+import com.tallerwebi.dominio.repository.FormacionEquipoRepository;
 import com.tallerwebi.presentacion.dto.EquipoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,13 +16,15 @@ import java.util.stream.Collectors;
 @Transactional
 public class EquipoServiceImpl implements EquipoService{
 
+   private final FormacionEquipoRepository formacionEquipoRepository;
    private final EquipoRepository repository;
    private final UsuarioService usuarioService;
 
    @Autowired
-   public EquipoServiceImpl(EquipoRepository repository, UsuarioService usuarioService) {
+   public EquipoServiceImpl(EquipoRepository repository, UsuarioService usuarioService, FormacionEquipoRepository formacionEquipoRepository) {
       this.repository = repository;
       this.usuarioService = usuarioService;
+      this.formacionEquipoRepository = formacionEquipoRepository;
    }
 
    @Override
@@ -65,6 +66,26 @@ public class EquipoServiceImpl implements EquipoService{
       usuarioService.actualizar(usuario);
    }
 
+   private List<Jugador> obtenerPlantilla(Equipo equipo){
+      List<FormacionEquipo> formacionesDelEquipo = this.formacionEquipoRepository.findByEquipoId(equipo.getId());
+      List<Jugador> jugadoresPlantillaEquipo = new ArrayList<>();
+      for(FormacionEquipo fe : formacionesDelEquipo){
+         jugadoresPlantillaEquipo.add(fe.getJugador());
+      }
+      return jugadoresPlantillaEquipo;
+   }
+
+   @Override
+   public Double calcularRatingEquipo(Equipo equipo){
+      Double rating = 0d;
+
+      List<Jugador> jugadoresPlantilla = obtenerPlantilla(equipo);
+
+      for(Jugador j : jugadoresPlantilla){
+         rating += j.getRating();
+      }
+      return rating / jugadoresPlantilla.size();
+   }
 
 
    private boolean isValid(EquipoDTO equipo){
